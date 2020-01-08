df8828d0c970424aa67c5bccd06377a7ac0348c1
kriluk12 <42153201+kriluk12@users.noreply.github.com>
2020-01-08 21:43:06 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -43,10 +43,10 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
-	filter "configurations:Debug*"
+	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "On"
 
-	filter "configurations:Release*"
+	filter "configurations:Release"
 		runtime "Release"
 		optimize "On"
